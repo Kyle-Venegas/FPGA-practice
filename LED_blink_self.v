8afa40371772f9dd529fcdc31baaf6f291b659d2
Kyle-Venegas <venegas.kyle@gmail.com>
2022-10-07 18:34:53 -0700

    initial file upload

---
+++ LED_blink_self.v
@@ -0,0 +1,8 @@
+// make 4 LEDs blink at a rate of 10Hz, 5Hz, 2Hz, 1Hz
+module LED_blink
+#(
+  parameter g_COUNT_10HZ  = 1250000,
+  parameter g_COUNT_5HZ   = 250000,
+  parameter g_COUNT_2HZ   = 6250000,
+  parameter g_COUNT_1HZ   = 
+
