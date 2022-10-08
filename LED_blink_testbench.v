969b8b1edb45f6938359b3657e53d3fa5a8938df
Kyle-Venegas <venegas.kyle@gmail.com>
2022-10-07 21:52:14 -0700

    testbench

--- LED_blink_testbench.v
+++ LED_blink_testbench.v
@@ -4,9 +4,9 @@
 
 `timescale 1ns/10ps
 
-module LED_blink_TB (); // just need to reference main module in testbench code
+module LED_blink_TB ();
 
-reg r_clk = 1'b0;
+reg r_Clk = 1'b0;
 
 // always clk = ~clk;
 // never executed, always at 0 time units
@@ -15,6 +15,21 @@ reg r_clk = 1'b0;
 // executes every 10 time units, acting as time delay
 // needs #x when testbenching
 
-always #1 r_clk = ~ r_clk;
+always #1 r_Clk = ~r_Clk;
 
 LED_blink 
+#(
+    .g_COUNT_10HZ(),
+    .g_COUNT_5HZ(),
+    .g_COUNT_2HZ(),
+    .g_COUNT_10HZ()
+) LED_blink_inst
+(
+    .i_Clk(r_Clk),
+    .o_LED_1(),
+    .o_LED_2(),
+    .o_LED_4(),
+    .o_LED_4()
+);
+
+endmodule
