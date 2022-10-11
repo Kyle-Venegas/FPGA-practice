0e9929ffaf545221b830fd817c6ef0145b29eb07
Kyle-Venegas <venegas.kyle@gmail.com>
2022-10-10 17:07:28 -0700

    comment

--- UART/UART_TB.v
+++ UART/UART_TB.v
@@ -15,6 +15,8 @@ module UART_RX_TB ();
     wire [7:0] w_RX_Byte;
 
     // task: use when procedure has any timing ctrl constructs, zero/more outputs, 1/more inputs
+    // works like a function written in the same file, not like modules
+    // takes in byte in hex, serializes it, passes it 1 by 1
     task UART_Write_Byte;
         input [7:0] i_Data;
         begin
@@ -52,7 +54,7 @@ module UART_RX_TB ();
     initial begin
         // send command to UART, sends ONE, 
         @(posedge r_Clk);
-        UART_WRITE_BYTE(8'h37); // sends byte 37 in hex
+        UART_WRITE_BYTE(8'h37); // sends byte 37 in hex, passed in as arg
         @(posedge r_Clk);
 
         // check if correct byte was received
