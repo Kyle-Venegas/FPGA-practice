c1c429c3f5b9fd58d7921c11843bc5a080162167
Kyle-Venegas <52269110+Kyle-Venegas@users.noreply.github.com>
2022-10-12 14:34:10 -0700

    UART TX initial

--- UART2/UART2.v
+++ UART2/UART2.v
@@ -1,2 +1,24 @@
-/*Nandland Project 8: UART (Universal Asynchronous Receiver Transmitter)
+/* Nandland Project 8: UART (Universal Asynchronous Receiver Transmitter)
 - UART1 only received, this one will also transmit
+- data displayed by 2 digit 7 segment display are ASCII hex characters of key pressed
+- this proj takes parallel data (as a byte) and serializes it, gets sent 1 bit at a time to computer
+  then is displayed in terminal emulator
+
+Proj desc: create loopback of data sent by computer. Go board receives data from the computer,
+display it on two digit 7 segment display, transmit data back to the computer. 
+- 115200 baud rate, 8 data bits, no parity, 1 stop bit, no flow control. */
+
+module UART_TX #(
+  parameter CLKS_PER_BIT = 217
+) (
+  input       i_rst,
+  input       i_clk,
+  input       i_tx_dv,
+  input [7:0] i_tx_byte,
+  output reg  o_tx_byte,
+  output reg  o_tx_active,
+  output reg  o_tx_serial,
+  output reg  o_tx_done
+);
+  
+endmodule
