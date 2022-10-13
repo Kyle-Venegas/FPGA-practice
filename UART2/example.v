557618370a05c9417db0e8c67cc698c6fc2dc8bf
Kyle-Venegas <52269110+Kyle-Venegas@users.noreply.github.com>
2022-10-13 13:07:50 -0700

    example

--- UART2/example.v
+++ UART2/example.v
@@ -1,130 +1,70 @@
-module UART_TX 
-  #(parameter CLKS_PER_BIT = 217)
-  (
-   input       i_Rst_L,
-   input       i_Clock,
-   input       i_TX_DV,
-   input [7:0] i_TX_Byte, 
-   output reg  o_TX_Active,
-   output reg  o_TX_Serial,
-   output reg  o_TX_Done
-   );
- 
-  localparam IDLE         = 2'b00;
-  localparam TX_START_BIT = 2'b01;
-  localparam TX_DATA_BITS = 2'b10;
-  localparam TX_STOP_BIT  = 2'b11;
+`timescale 1ns/10ps
+
+`include "UART_TX.v"
+
+module UART_TB ();
+
+  // Testbench uses a 25 MHz clock
+  // Want to interface to 115200 baud UART
+  // 25000000 / 115200 = 217 Clocks Per Bit.
+  parameter c_CLOCK_PERIOD_NS = 40;
+  parameter c_CLKS_PER_BIT    = 217;
+  parameter c_BIT_PERIOD      = 8600;
   
-  reg [2:0] r_SM_Main;
-  reg [$clog2(CLKS_PER_BIT):0] r_Clock_Count;
-  reg [2:0] r_Bit_Index;
-  reg [7:0] r_TX_Data;
+  reg r_Clock = 0;
+  reg r_TX_DV = 0;
+  wire w_TX_Active, w_UART_Line;
+  wire w_TX_Serial;
+  reg [7:0] r_TX_Byte = 0;
+  wire [7:0] w_RX_Byte;
 
+  UART_RX #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_RX_Inst
+    (.i_Clock(r_Clock),
+     .i_RX_Serial(w_UART_Line),
+     .o_RX_DV(w_RX_DV),
+     .o_RX_Byte(w_RX_Byte)
+     );
+  
+  UART_TX #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_TX_Inst
+    (.i_Clock(r_Clock),
+     .i_TX_DV(r_TX_DV),
+     .i_TX_Byte(r_TX_Byte),
+     .o_TX_Active(w_TX_Active),
+     .o_TX_Serial(w_TX_Serial),
+     .o_TX_Done()
+     );
 
-  // Purpose: Control TX state machine
-  always @(posedge i_Clock or negedge i_Rst_L)
-  begin
-    if (~i_Rst_L)
+  // Keeps the UART Receive input high (default) when
+  // UART transmitter is not active
+  assign w_UART_Line = w_TX_Active ? w_TX_Serial : 1'b1;
+    
+  always
+    #(c_CLOCK_PERIOD_NS/2) r_Clock <= !r_Clock;
+  
+  // Main Testing:
+  initial
     begin
-      r_SM_Main <= 3'b000;
+      // Tell UART to send a command (exercise TX)
+      @(posedge r_Clock);
+      @(posedge r_Clock);
+      r_TX_DV   <= 1'b1;
+      r_TX_Byte <= 8'h3F;
+      @(posedge r_Clock);
+      r_TX_DV <= 1'b0;
+
+      // Check that the correct command was received
+      @(posedge w_RX_DV);
+      if (w_RX_Byte == 8'h3F)
+        $display("Test Passed - Correct Byte Received");
+      else
+        $display("Test Failed - Incorrect Byte Received");
+      $finish();
     end
-    else
-    begin
-
-      o_TX_Done <= 1'b0;
-
-      case (r_SM_Main)
-      IDLE :
-        begin
-          o_TX_Serial   <= 1'b1;         // Drive Line High for Idle
-          r_Clock_Count <= 0;
-          r_Bit_Index   <= 0;
-          
-          if (i_TX_DV == 1'b1)
-          begin
-            o_TX_Active <= 1'b1;
-            r_TX_Data   <= i_TX_Byte;
-            r_SM_Main   <= TX_START_BIT;
-          end
-          else
-            r_SM_Main <= IDLE;
-        end // case: IDLE
-      
-      
-      // Send out Start Bit. Start bit = 0
-      TX_START_BIT :
-        begin
-          o_TX_Serial <= 1'b0;
-          
-          // Wait CLKS_PER_BIT-1 clock cycles for start bit to finish
-          if (r_Clock_Count < CLKS_PER_BIT-1)
-          begin
-            r_Clock_Count <= r_Clock_Count + 1;
-            r_SM_Main     <= TX_START_BIT;
-          end
-          else
-          begin
-            r_Clock_Count <= 0;
-            r_SM_Main     <= TX_DATA_BITS;
-          end
-        end // case: TX_START_BIT
-      
-      
-      // Wait CLKS_PER_BIT-1 clock cycles for data bits to finish         
-      TX_DATA_BITS :
-        begin
-          o_TX_Serial <= r_TX_Data[r_Bit_Index];
-          
-          if (r_Clock_Count < CLKS_PER_BIT-1)
-          begin
-            r_Clock_Count <= r_Clock_Count + 1;
-            r_SM_Main     <= TX_DATA_BITS;
-          end
-          else
-          begin
-            r_Clock_Count <= 0;
-            
-            // Check if we have sent out all bits
-            if (r_Bit_Index < 7)
-            begin
-              r_Bit_Index <= r_Bit_Index + 1;
-              r_SM_Main   <= TX_DATA_BITS;
-            end
-            else
-            begin
-              r_Bit_Index <= 0;
-              r_SM_Main   <= TX_STOP_BIT;
-            end
-          end 
-        end // case: TX_DATA_BITS
-      
-      
-      // Send out Stop bit.  Stop bit = 1
-      TX_STOP_BIT :
-        begin
-          o_TX_Serial <= 1'b1;
-          
-          // Wait CLKS_PER_BIT-1 clock cycles for Stop bit to finish
-          if (r_Clock_Count < CLKS_PER_BIT-1)
-          begin
-            r_Clock_Count <= r_Clock_Count + 1;
-            r_SM_Main     <= TX_STOP_BIT;
-          end
-          else
-          begin
-            o_TX_Done     <= 1'b1;
-            r_Clock_Count <= 0;
-            r_SM_Main     <= IDLE;
-            o_TX_Active   <= 1'b0;
-          end 
-        end // case: TX_STOP_BIT      
-      
-      default :
-        r_SM_Main <= IDLE;
-      
-    endcase
-    end // else: !if(~i_Rst_L)
-  end // always @ (posedge i_Clock or negedge i_Rst_L)
-
   
+  initial 
+  begin
+    // Required to dump signals to EPWave
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
 endmodule
