29491cebd70332955fe6963318cb368fa4de3ff8
Kyle-Venegas <venegas.kyle@gmail.com>
2022-10-13 16:15:44 -0700

    UART2_TB

--- UART2/UART2_TB.v
+++ UART2/UART2_TB.v
@@ -8,40 +8,15 @@ module UART_TB ();
     parameter c_CLK_PERIOD_NS   = 40;       // need explanation
     parameter c_BIT_PERIOD      = 8600;     // need explanation
 
-    reg r_clk = 0;
-    reg r_rx_serial = 0;
+    reg       r_clk = 0;
+    reg       r_tx_dv = 0;
+    reg       r_rx_serial = 0;
+    reg [7:0] r_tx_byte = 0;
 
     wire        w_tx_active;
     wire        w_uart_line;
     wire        w_tx_serial;
-
-    reg   [7:0] r_tx_byte = 0;
     wire  [7:0] w_rx_byte;
-
-    // task: use when procedure has any timing ctrl constructs, zero/more outputs, 1/more inputs
-    // does not have to return anything
-    // works like a function written in the same file, not like modules
-    // takes in byte in hex, serializes it, passes it 1 by 1
-//  task UART_Write_Byte;
-//      input [7:0] i_Data;
-//      integer i;
-//      begin
-//          // send start bit
-//          r_rx_serial <= 1'b0;
-//          #(c_BIT_PERIOD);
-//          #1000;
-
-//          // send data byte
-//          for (i=0; i<8; i=i+1) begin
-//              r_rx_serial <= i_Data[i];
-//              #(c_BIT_PERIOD);
-//          end
-
-//          // send stop bit
-//          r_rx_serial <= 1'b1;
-//          #(c_BIT_PERIOD);
-//      end
-//  endtask
 
     UART_RX #(
       .CLKS_PER_BIT(c_CLKS_PER_BIT)
@@ -55,15 +30,15 @@ module UART_TB ();
     UART_TX #(
       .CLKS_PER_BIT(c_CLKS_PER_BIT)
     ) UART_TX_INST (
-      .i_rst,        // added reset input
       .i_clk(r_clk),
-      .i_tx_dv,
-      .i_tx_byte,
-      .o_tx_byte,
-      .o_tx_active,
-      .o_tx_serial,
-      .o_tx_done
+      .i_tx_dv(r_tx_dv),
+      .i_tx_byte(r_tx_byte),
+      .o_tx_active(w_tx_active),
+      .o_tx_serial(w_tx_serial),
+      .o_tx_done()
     );
+
+    assign w_uart_line = w_tx_active ? w_tx_serial : 1'b1;
 
     always #(c_CLK_PERIOD_NS/2) r_clk <= !r_clk;
     // ~ bit wise op, returns invert of arg
@@ -71,13 +46,16 @@ module UART_TB ();
 
     // main test
     initial begin
-        // send command to UART, sends ONE, 
         @(posedge r_clk);
-        UART_Write_Byte(8'h37); // sends byte 37 in hex, passed in as arg
-        @(posedge r_clk);       // why trigger posedge twice?
+        @(posedge r_clk);
+        r_tx_dv <= 1'b1;
+        r_tx_byte <= 8'h3F;
+        @(posedge r_clk);
+        r_tx_dv <= 1'b0;
 
         // check if correct byte was received
-        if (w_rx_byte == 8'h37)
+        @(posedge r_clk);
+        if (w_rx_byte == 8'h3F)
             $display("Correct byte received");
         else
             $display("Wrong byte received");
