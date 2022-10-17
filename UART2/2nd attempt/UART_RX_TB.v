0619437adcd205609ffbf45163046d0035a62c55
Kyle-Venegas <52269110+Kyle-Venegas@users.noreply.github.com>
2022-10-16 20:36:48 -0700

    tb test

--- UART2/2nd attempt/UART_RX_TB.v
+++ UART2/2nd attempt/UART_RX_TB.v
@@ -11,17 +11,10 @@ module UART_RX_TB ();
 
   wire [7:0] rx_byte;
 
-  UART_RX #(CLKS_PER_BIT = 217) UART_RX_INST (
-    .clk(clk),         
-    .serial_stream(serial_stream),
-    .rx_byte(rx_byte),
-    .data_valid()
-  );
-
   // has to be a task because of stream must be sent every clock cycle
   task write_byte;
     input [7:0] data;
-    integer i = 0;
+    integer i;
     begin
       // send start bit
       serial_stream <= 1'b0;
@@ -29,7 +22,7 @@ module UART_RX_TB ();
       #1000;
 
       // send byte
-      for (i=0, i<8, i=i+1) begin
+      for (i=0; i<8; i=i+1) begin
         serial_stream <= data[i];
         #(BIT_PERIOD);
       end
@@ -40,6 +33,13 @@ module UART_RX_TB ();
     end
   endtask
 
+  UART_RX #(.CLKS_PER_BIT(CLKS_PER_BIT)) UART_RX_INST (
+    .clk(clk),         
+    .serial_stream(serial_stream),
+    .rx_byte(rx_byte),
+    .data_valid()
+  );
+
   always #(CLK_PERIOD_NS/2) clk <= !clk;
 
   initial begin
@@ -47,11 +47,11 @@ module UART_RX_TB ();
     write_byte(8'h37);
     @(posedge clk);
 
-    if (rx_byte)
+    if (rx_byte == 8'h37)
       $display("Correct byte recvd");
     else
       $display("wrong byte recvd");
-    $finish
+    $finish;
   end
 
   initial begin
