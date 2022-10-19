b56ad1084b7b1b143269e279757b9758a71f9db0
Kyle-Venegas <venegas.kyle@gmail.com>
2022-10-19 16:15:22 -0700

    TX io

--- UART2/2nd attempt/UART_TB.v
+++ UART2/2nd attempt/UART_TB.v
@@ -14,17 +14,21 @@ module UART_TB ();
   wire [7:0] rx_byte       = 0;
   wire       rx_dv         = 0;
 
+  // TX I/O
+  wire tx_serial_stream = 0;
+  wire tx_active        = 0;
+
   UART_RX #(.CLKS_PER_BIT(CLKS_PER_BIT)) UART_RX_INST (
     .clk(clk),
     .serial_stream(serial_stream),
     .rx_byte(rx_byte),
-    .data_valid(w_rx_dv);
+    .data_valid(rx_dv);
   );
 
   UART_TX #(.CLKS_PER_BIT(CLKS_PER_BIT)) UART_TX_INST (
     .clk(clk),
-    .rx_dv(r_rx_dv), 
-    .rx_byte(r_rx_byte),
+    .rx_dv(rx_dv), 
+    .rx_byte(rx_byte),
     .tx_serial(tx_serial_stream),
     .tx_active(tx_active),
     .tx_done()
@@ -33,8 +37,6 @@ module UART_TB ();
   always #(CLK_PERIOD_NS/2) clk <= !clk;
 
   initial begin
-    @(posedge clk);
-    @(posedge clk);
 
   end
 
