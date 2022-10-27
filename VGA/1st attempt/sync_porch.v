63726181a649700fb093218b4bcca381e59b79ca
Kyle-Venegas <venegas.kyle@gmail.com>
2022-10-27 15:48:56 -0700

    comments

--- VGA/1st attempt/sync_porch.v
+++ VGA/1st attempt/sync_porch.v
@@ -1,1 +1,7 @@
 // modifies hsync and vsync to include front and back porch.
+// porches meant more back when monitors used Cathode Ray tubes to draw 
+//  image on screen.
+// front and back porch maintained due to convention, not monitor anatomy
+
+// DVI and HDMI being meant for digital signals, porches concept are 
+//  removed
