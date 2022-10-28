780da2c04f0ee291d4da10478cf399b8337c78d2
Kyle-Venegas <venegas.kyle@gmail.com>
2022-10-28 15:47:40 -0700

    i/o

--- VGA/1st attempt/VGA_pattern_main.v
+++ VGA/1st attempt/VGA_pattern_main.v
@@ -2,4 +2,33 @@ module VGA_pattern_main (
   input  i_Clk,
   input  i_UART_RX,
   output o_UART_RX,
-  )
+  // Segment 1
+  output o_Segment1_A,
+  output o_Segment1_B,
+  output o_Segment1_C,
+  output o_Segment1_D,
+  output o_Segment1_E,
+  output o_Segment1_F,
+  output o_Segment1_G,
+  // segment 2
+  output o_Segment2_A,
+  output o_Segment2_B,
+  output o_Segment2_C,
+  output o_Segment2_D,
+  output o_Segment2_E,
+  output o_Segment2_F,
+  output o_Segment2_G,
+  // VGA
+  output o_VGA_hsync,
+  output o_VGA_HSync,
+  output o_VGA_VSync,
+  output o_VGA_Red_0,
+  output o_VGA_Red_1,
+  output o_VGA_Red_2,
+  output o_VGA_Grn_0,
+  output o_VGA_Grn_1,
+  output o_VGA_Grn_2,
+  output o_VGA_Blu_0,
+  output o_VGA_Blu_1,
+  output o_VGA_Blu_2
+  );
