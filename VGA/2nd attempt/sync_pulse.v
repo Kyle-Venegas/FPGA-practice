8ad975d5e10f9d5e6bc1802caa708f61af7c524d
Kyle-Venegas <venegas.kyle@gmail.com>
2022-11-03 17:27:59 -0700

    sync generator

--- VGA/2nd attempt/sync_pulse.v
+++ VGA/2nd attempt/sync_pulse.v
@@ -16,16 +16,34 @@ module sync_pulse #(
   output reg [9:0] o_row_counter = 0
   );
   
+/* due to FPGA's procedural nature to the clock,
+   the code below will not work. Code would have 
+   worked if this was a while loop
+   first output would have been col: 1, row: 1
+   which would have been wrong */ 
+//  always @(posedge clk ) begin
+//    if (o_col_counter < TOTAL_COLS-1) begin
+//      if (o_row_counter < TOTAL_ROWS-1) begin
+//        o_row_counter <= o_row_counter + 1;
+//      end else begin
+//        o_row_counter <= 0;
+//      end
+//      o_col_counter <= o_col_counter + 1;
+//    end else begin
+//      o_col_counter <= 0;
+//    end
+//  end
+
   always @(posedge clk ) begin
-    if (o_col_counter < TOTAL_COLS-1) begin
-      if (o_row_counter < TOTAL_ROWS-1) begin
-        o_row_counter <= o_row_counter + 1;
-      end else begin
-        o_row_counter <= 0;
-      end
-      o_col_counter <= o_col_counter + 1;
-    end else begin
+    if (o_col_counter == TOTAL_COLS-1) begin
       o_col_counter <= 0;
+      if (o_row_counter == TOTAL_ROWS-1) begin
+        o_row_counter <= 0;
+      end else begin
+        o_row_counter <= o_row_counter + 1;
+      end
+    end else begin
+      o_col_counter <= o_col_counter + 1;
     end
   end
 
