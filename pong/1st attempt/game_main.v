5dc7650df1285f981b3ca89a6b547cc77b328e16
Kyle-Venegas <52269110+Kyle-Venegas@users.noreply.github.com>
2022-11-11 16:37:49 -0800

    modules

--- pong/1st attempt/game_main.v
+++ pong/1st attempt/game_main.v
@@ -30,6 +30,11 @@ module game_main #(
   wire       w_hsync, w_vsync;
   wire [9:0] w_col_counter, w_row_counter;
 
+  // paddle wires, i/o
+  wire [9:0] w_paddle_y1, w_paddle_y2;
+  wire [9:0] w_draw1, w_draw2;
+  wire [5:0] w_col_counter_div, w_row_counter_div;
+
   sync_counter #(
     .TOTAL_COLS(TOTAL_COLS),
     .TOTAL_ROWS(TOTAL_ROWS))
@@ -47,20 +52,54 @@ module game_main #(
     o_vsync <= w_vsync;
   end
 
-  paddle #(
-    .TOTAL_COLS(),
-    .TOTAL_ROWS()) 
-  P1 (
-    clk           (clk)       ,
-    i_player_index(0)         ,
-    i_up          (i_switch_1),
-    i_down        (i_switch_2),
-    o_paddle_y    ()          , // output reg
-    o_draw        ()    );
+  // take only 4 bits from the 10 bits. 640/16 = 40; 480/16 = 30;
+  // essentially division
+  assign w_col_counter_div = w_col_counter[9:4];
+  assign w_row_counter_div = w_row_counter[9:4];
 
-  
+  paddle P1 (
+    .clk              (clk)              ,
+    .i_player_index   (0)                ,
+    .i_up             (i_switch_1)       ,
+    .i_down           (i_switch_2)       ,
+    .i_col_counter_div(w_col_counter_div),
+    .i_row_counter_div(w_row_counter_div),
+    .o_paddle_y       (w_paddle_y1)      ,   // output reg
+    .o_draw           (w_draw1)          );  // output reg
 
+  paddle P2 (
+    .clk              (clk)              ,
+    .i_player_index   (ACTIVE_COLS-1)    ,
+    .i_up             (i_switch_3)       ,
+    .i_down           (i_switch_4)       ,
+    .i_col_counter_div(w_col_counter_div),
+    .i_row_counter_div(w_row_counter_div),
+    .o_paddle_y       (w_paddle_y2)      ,   // output reg
+    .o_draw           (w_draw2)          );  // output reg
 
+  draw_ball ball (
+    .clk              (clk)              ,
+    .i_paddle_y1      (w_paddle_y_1)     ,
+    .i_paddle_y2      (w_paddle_y_2)     ,
+    .i_col_counter_div(w_col_counter_div),
+    .i_row_counter_div(w_row_counter_div),
+    .o_draw_ball      (w_draw_ball)      ,
+    .o_draw_x         (w_draw_x)         ,   // output reg
+    .o_draw_y         (w_draw_y)         );  // output reg
+
+  // state machine
+  always @(posedge clk ) begin
+    case (STATE)
+
+      IDLE: begin
+        
+      end
+      
+      default: begin
+        STATE <= IDLE;
+      end
+    endcase
+  end
 
 
 
