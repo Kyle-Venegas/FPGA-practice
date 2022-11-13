a5403864a88bc245b34aea3ed3397178be807293
Kyle-Venegas <venegas.kyle@gmail.com>
2022-11-12 19:22:55 -0800

    paddle speed param

--- pong/1st attempt/paddle.v
+++ pong/1st attempt/paddle.v
@@ -9,7 +9,15 @@ module paddle #(
   input [5:0]      i_row_counter_div,
   output reg [5:0] o_paddle_y       ,   // output reg
   output reg       o_draw           );  // output reg
+  // o_paddle_y should be an array for the ball to bounce off of
 
+  // how to make paddle move while button is being pressed?
 
+  // like LED blink: 
+  // 1Hz  = 12500000
+  // 10Hz = 1250000
+  parameter PADDLE_SPEED = 1250000;
+
+  
 
 endmodule
